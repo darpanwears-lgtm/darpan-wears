b6a477eef2734e872cc36249146e7469d6f764bc
darpan wears <darpanwears@gmail.com>
2025-11-06 18:51:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5240037082-602d2",
+  "appId": "1:480800852343:web:3ced2d95dde14cde5b3972",
+  "apiKey": "AIzaSyDhHoJNBjoZVLymz9UIjNCrYSX1tE8u5-M",
+  "authDomain": "studio-5240037082-602d2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "480800852343"
+};
